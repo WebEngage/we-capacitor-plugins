a8e2a623c706da8177c1fa9ba94b99b592c343e0
Milind Keni <milind.keni@webengage.com>
2024-08-28 16:13:07 +0530

    initial commit for capacitor webengage

---
+++ ngx/index.d.ts
@@ -0,0 +1,166 @@
+import { AwesomeCordovaNativePlugin } from '@awesome-cordova-plugins/core';
+/**
+ * @name Webengage
+ * @description
+ * Awesome Cordova Plugins wrapper that wraps Webengage Cordova plugin for Android and iOS
+ * @usage
+ * ```typescript
+ * import { Webengage, WebengageUser, WebengagePush, WebengageNotification, WebengageJwtManager  } from '@awesome-cordova-plugins/webengage/ngx';
+ *
+ *
+ * constructor(private webengage: Webengage, private webengageUser: WebengageUser, private webengagePush: WebengagePush, private webengageNotification: WebengageNotification ) { }
+ *
+ * ...
+ *
+ * this.webengage.engage();
+ * ```
+ */
+export declare class Webengage extends AwesomeCordovaNativePlugin {
+    /**
+     * Initializes WebEngage SDK
+     * @param {any} [config]
+     * @returns {Promise<any>}
+     */
+    engage(config?: any): Promise<any>;
+    /**
+     * Starts Tracking Google Advertising ID
+     * @returns {Promise<any>}
+     */
+    startGAIDTracking(): Promise<any>;
+    /**
+     * Sets WebEngage SDK configuration
+     * @param {string} key
+     * @param {any} value
+     * @returns {Promise<any>}
+     */
+    options(key: string, value: any): Promise<any>;
+    /**
+     * Tracks event
+     * @param {string} eventName
+     * @param {any} [attributes]
+     * @returns {Promise<any>}
+     */
+    track(eventName: string, attributes?: any): Promise<any>;
+    /**
+     * Tracks screen
+     * @param {string} eventName
+     * @param screenName
+     * @param {any} [screenData]
+     * @returns {Promise<any>}
+     */
+    screen(screenName: string, screenData?: any): Promise<any>;
+}
+/**
+ * @hidden
+ */
+export declare class WebengageUser extends AwesomeCordovaNativePlugin {
+    /**
+     * Logs user in
+     * @param {string} userId
+     * @returns {Promise<any>}
+     */
+    login(userId: string, jwtToken?: string): Promise<any>;
+    /**
+     * Logs user in
+     * @param {string} userId
+     * @returns {Promise<any>}
+     */
+    setSecureToken(userId: string, jwtToken: string): Promise<any>;
+    /**
+     * Logs user out
+     * @returns {Promise<any>}
+     */
+    logout(): Promise<any>;
+    /**
+     * Sets user attribute
+     * @param {string} key
+     * @param {any} value
+     * @returns {Promise<any>}
+     */
+    setAttribute(key: string, value: any): Promise<any>;
+    /**
+     * Sets Device optIn
+     * @param {boolean} optIn
+     * @returns {Promise<any>}
+     */
+    setDevicePushOptIn(optIn: boolean): Promise<any>;
+    /**
+     * Sets user attribute
+     * @param {string} channel
+     * @param {any} optIn
+     * @returns {Promise<any>}
+     */
+    setUserOptIn(channel: string, optIn: any): Promise<any>;
+    /**
+     * Sets user location
+     * @param {number} latitude
+     * @param {number} longitude
+     * @returns {Promise<any>}
+     */
+    setLocation(latitude: number, longitude: number): Promise<any>;
+}
+/**
+ * @hidden
+ */
+export declare class WebengageJwtManager extends AwesomeCordovaNativePlugin {
+    /**
+     * Callback function is invoked when a Jwt token is clicked
+     * @param {any} callback
+     * @returns {Promise<any>}
+     */
+    tokenInvalidatedCallback(callback: any): Promise<any>;
+}
+/**
+ * @hidden
+ */
+export declare class WebengagePush extends AwesomeCordovaNativePlugin {
+    /**
+     * Callback function is invoked when a push notification is clicked
+     * @param {any} callback
+     * @returns {Promise<any>}
+     */
+    onClick(callback: any): Promise<any>;
+    /**
+     * Sets push notification configuration
+     * @param {string} key
+     * @param {any} value
+     * @returns {Promise<any>}
+     */
+    options(key: string, value: any): Promise<any>;
+}
+/**
+ * @hidden
+ */
+export declare class WebengageNotification extends AwesomeCordovaNativePlugin {
+    /**
+     * Callback function is invoked when a in-app notification is shown
+     * @param {any} callback
+     * @returns {Promise<any>}
+     */
+    onShown(callback: any): Promise<any>;
+    /**
+     * Callback function is invoked before a in-app notification is shown
+     * @param {any} callback
+     * @returns {Promise<any>}
+     */
+    onPrepared(callback: any): Promise<any>;
+    /**
+     * Callback function is invoked when a in-app notification is clicked
+     * @param {any} callback
+     * @returns {Promise<any>}
+     */
+    onClick(callback: any): Promise<any>;
+    /**
+     * Callback function is invoked when a in-app notification is dismissed
+     * @param {any} callback
+     * @returns {Promise<any>}
+     */
+    onDismiss(callback: any): Promise<any>;
+    /**
+     * Sets in-app notification configuration
+     * @param {string} key
+     * @param {any} value
+     * @returns {Promise<any>}
+     */
+    options(key: string, value: any): Promise<any>;
+}
